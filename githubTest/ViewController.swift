b44a4fa295162281b29816dd378d3871accdc61c
wulong yang <yangwulong0418@gmail.com>
2017-02-06 11:24:03 +1100

    6666

--- githubTest/ViewController.swift
+++ githubTest/ViewController.swift
@@ -24,7 +24,7 @@ class ViewController: UIViewController {
         var i = 500
         
         var e = 3333 eeee
-        var  f  =444jjjjhhh
+        var  f  =444jjjjhhh666
     }
 
     override func didReceiveMemoryWarning() {
